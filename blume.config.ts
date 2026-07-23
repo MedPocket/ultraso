2fcd91af5a317d81138eccd29dac6fe56284da32
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-07-23 08:48:34 +0700

    Document for clinical ultrasound

--- blume.config.ts
+++ blume.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "blume";
 
 export default defineConfig({
   title: "Ultraso",
-  description: "Tài liệu về vật lý siêu âm lâm sàng, kỹ thuật quét và ứng dụng.",
+  description: "Tài liệu về siêu âm lâm sàng.",
 
   feedback: false,
 
